0eea24672ace59f56738167b9410d591bc5a2c32
RZiln <radvilas.zilinskas@gmail.com>
2026-09-05 15:05:33 +0300

    merge

--- app/src/main/java/com/example/androidhelloworld/MainActivity.java
+++ app/src/main/java/com/example/androidhelloworld/MainActivity.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Comment for revert
     TextView changableText;
     Button button;
     Button button2;
