982b9992e8953d7ab39116e2b4eee07f63bf0de5
RZiln <radvilas.zilinskas@gmail.com>
2026-09-05 14:44:56 +0300

    Button and Textview added, Button changes TextView text from TextList

--- app/src/main/java/com/example/androidhelloworld/MainActivity.java
+++ app/src/main/java/com/example/androidhelloworld/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidhelloworld;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView changableText;
+    Button button;
+
+    String[] textList = {"Text 1", "Text 2", "Text 3"};
+    int textIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        changableText = findViewById(R.id.changableText);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changableText.setText(textList[textIndex]);
+                textIndex += 1;
+                if (textIndex >= textList.length) {
+                    textIndex = 0;
+                }
+            }
+        });
+
     }
 }
